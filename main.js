07a0988ba0876f4d817559c9bae56a29767abd41
Jessica Fung <jessicafung@school.local>
2025-08-05 16:55:59 -0700

    initial code

---
+++ main.js
@@ -0,0 +1,7 @@
+function sayYes(){
+    document.getElementById("dialogue").innerText="Bugsy: Great! A variable is like a box. You name it and put stuff in it!\nTry this:\n\nlet snack = 'cookie';\nconsole.log(snack);";
+}
+
+function sayNo(){
+    document.getElementById("dialogue").innerText="Awe okay, Bugsy sad 🍪🥲";
+}
