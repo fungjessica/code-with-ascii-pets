1088ed0b60e5af2e5a3c1faa4eee05eec125a78d
Jessica Fung <jessicafung@school.local>
2025-08-05 19:31:14 -0700

    example and quiz added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function sayYes(){
     document.getElementById("console-section").style.display = "block";
-    document.getElementById("dialogue").innerText="Bugsy: Great! A variable is like a box. You name it and put stuff in it!\nTry this:\n\nlet snack = 'cookie';\nconsole.log(snack);";
+    document.getElementById("examples").style.display = "block";
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("dialogue").innerText = 
+        "Bugsy: Great! A variable is like a box. You name it and put stuff in it!\nTry this:\n\nlet snack = 'cookie';\nconsole.log(snack);";
 }
 
 function sayNo(){
+    document.getElementById("console-section").style.display = "none";
     document.getElementById("dialogue").innerText="Awe okay, Bugsy sad 🍪🥲";
 }
 
@@ -16,6 +20,11 @@ function runCode() {
     const originalConsoleLog = console.log;
     console.log = function(message) {
         output.textContent += message + '\n';
+
+        if (message === "cookie") {
+            document.getElementById("examples").style.display = "none";
+            document.getElementById("quiz").style.display = "block";
+        }
     };
 
     try {
